4f1d2bda7bfde1740cf3a31b5ed71a47e8cb026c
Egor Tanachev <71014753+egortanachev@users.noreply.github.com>
2023-11-07 16:37:16 +0500

    [web development] – Update project

--- scripts/editForm.js
+++ scripts/editForm.js
@@ -48,16 +48,26 @@ document.addEventListener('DOMContentLoaded', function() {
 // Pop up
 const popupOpen = document.querySelector('.pop-up__open');
 const popupClose = document.querySelector('.pop-up__close');
+const popupClose2 = document.querySelector('.popup__dark');
 
-popupOpen.addEventListener('click', (event) => {
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+popupClose2.addEventListener('click', closePopup);
+
+function openPopup(event){
     event.preventDefault();
     document.querySelector('.body').classList.toggle('modal')
-});
+}
 
-popupClose.addEventListener('click', (event) => {
+function closePopup(event){
     event.preventDefault();
-    document.querySelector('.body').classList.toggle('modal')
-});
+    
+    document.querySelector('.body').classList.add('modal__close');
+    setTimeout(() => {
+        document.querySelector('.body').classList.remove('modal__close');
+        document.querySelector('.body').classList.toggle('modal')
+    }, 500);
+}
 
 
 // Redirect EDIT
